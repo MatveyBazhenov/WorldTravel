6b1307a50596f3db5ad3ee7a157ba657dc70ce28
Dasha <sdk062006@yandex.ru>
2025-03-21 21:36:31 +0300

    handle registration

--- ui/RegistrationWindow.cpp
+++ ui/RegistrationWindow.cpp
@@ -1,5 +1,9 @@
 #include "RegistrationWindow.hpp"
 
+wxBEGIN_EVENT_TABLE(RegistrationWindow, wxFrame)
+    EVT_BUTTON(ID_REG2, RegistrationWindow::OnRegisterButtonClicked)
+wxEND_EVENT_TABLE()
+
 RegistrationWindow::RegistrationWindow(wxWindow *parent)
     : wxFrame(
           parent,
@@ -33,4 +37,49 @@ RegistrationWindow::RegistrationWindow(wxWindow *parent)
     this->SetSizer(mainSizer2);
     Center();
     this->Fit();
+}
+
+void RegistrationWindow::OnRegisterButtonClicked(wxCommandEvent& event) {
+    wxString login = txtLogin->GetValue();
+    wxString password = txtPassword->GetValue();
+
+    // делаем json
+    wxString jsonBody = wxString::Format(
+        R"({"username": "%s", "password": "%s"})",
+        login, password
+    );
+
+    // отправляем на сервер запрос
+    wxHTTP http;
+    http.SetHeader("Content-Type", "application/json");
+    http.SetTimeout(10);
+
+    const wxString url = "http://localhost:8080/api/register";
+    wxInputStream* stream = nullptr;
+    
+    try {
+        if (http.Connect("localhost", 8080)) {
+            stream = http.PostRequest("/api/register", jsonBody.ToUTF8());
+        }
+
+        // смотрим ответ
+        const int responseCode = http.GetResponseCode();
+        wxString response;
+        
+        if (stream) {
+            wxStringOutputStream out(&response);
+            stream->Read(out);
+        }
+
+        if (responseCode == 201) {
+            wxMessageBox("Регистрация успешна!\nВаш ключ: " + response, "Успех", wxOK | wxICON_INFORMATION);
+        } else {
+            wxMessageBox("Ошибка: " + response, "Ошибка", wxOK | wxICON_ERROR);
+        }
+    } catch (...) {
+        wxMessageBox("Ошибка подключения к серверу", "Ошибка", wxOK | wxICON_ERROR);
+    }
+
+    if (stream) delete stream;
+    http.Close();
 }
--- ui/main.cpp
+++ ui/main.cpp
@@ -13,13 +13,9 @@ public:
     virtual bool OnInit();
 };
 
-wxBEGIN_EVENT_TABLE(SocketMain, wxApp)
-    EVT_SOCKET(SERVER_ID, SocketMain::ServerConnect)
-        EVT_SOCKET(SOCKET_ID, SocketMain::ServerSockedData) wxEND_EVENT_TABLE()
-            wxIMPLEMENT_APP(MyApp);
+wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit() {
-    SocketMain *socketMain = new SocketMain();
     MainWin *mainWin = new MainWin();
     mainWin->Show(true);
     return true;
