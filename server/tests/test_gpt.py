755c872ebac610dad4458b87ad8864f040e596e5
Milanya <yakovlevamilanya@yandex.ru>
2025-05-25 18:16:06 +0300

    test

--- server/tests/test_gpt.py
+++ server/tests/test_gpt.py
@@ -1,14 +1,14 @@
-# import pytest 
+import pytest 
 
-# @pytest.mark.asyncio
-# async def test_mock_gpt_response(mock_gpt, service_client):
-#     response = await service_client.get(
-#         '/gpt', 
-#         json={"origin":"Хабаровск"}
-#     )
-#     assert response.status == 200
-#     data = response.json()
+@pytest.mark.asyncio
+async def test_mock_gpt_response(mock_gpt, service_client):
+    response = await service_client.get(
+        '/gpt', 
+        json={"origin":"Хабаровск"}
+    )
+    assert response.status == 200
+    data = response.json()
 
-#     assert isinstance(data, list)
-#     assert len(data) == 5
+    assert isinstance(data, list)
+    assert len(data) == 5
     
